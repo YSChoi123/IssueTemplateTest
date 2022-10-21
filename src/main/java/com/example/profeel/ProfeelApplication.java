630ec79f50d0d47d848b8d32119437105073d716
yunseo <cysdl5604@inu.ac.kr>
2022-10-21 17:18:23 +0900

    init

---
+++ src/main/java/com/example/profeel/ProfeelApplication.java
@@ -0,0 +1,13 @@
+package com.example.profeel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfeelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfeelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/profeel/ProfeelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.profeel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfeelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
